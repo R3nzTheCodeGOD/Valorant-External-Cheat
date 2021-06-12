c930719bf8cdb34590f03370c472f4ca664319aa
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2021-06-12 03:56:19 +0300

    new config system fix (#25)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ SOFTWARE.
 
 from winsound import Beep
 from colorama import Fore, Style, init
-from mss import mss
+from mss import base, mss
 from PIL import ImageGrab, Image
 from ctypes import windll
 from time import perf_counter, sleep
@@ -32,15 +32,19 @@ from os import system
 from keyboard import is_pressed
 from hashlib import sha256
 from random import random
+from json import load, dump
 
 
 __author__   = 'R3nzTheCodeGOD'
-__version__  = 'v2.0.2'
+__version__  = 'v2.0.3'
 
 
 S_HEIGHT, S_WIDTH  = ImageGrab.grab().size
-GRABZONE           = 0x5
-TRIGGER_KEY        = 'shift'
+GRABZONE           = 5
+IS_HOLDKEY         = True
+IS_RUNING          = True
+HOLDKEY            = 'shift'
+TOGGLEKEY          = 'F6'
 SWITCH_KEY         = 'ctrl + tab'
 GRABZONE_KEY_UP    = 'ctrl + up'
 GRABZONE_KEY_DOWN  = 'ctrl + down'
@@ -51,17 +55,60 @@ class FoundEnemy(Exception):
     pass
 
 
+class Config:
+
+    base = {
+        'Grabzone'  : 5,
+        'IsHoldKey' : False,
+        'HoldKey'   : 'shift',
+        'ToggleKey' : 'F6'
+    }
+
+    def __init__(self, configName: str = 'config.json') -> None:
+        self.configName = configName
+        self.config     = None
+
+
+    def createBaseConfig(self) -> None:
+        with open(self.configName, 'w', encoding='utf-8') as f:
+            self.config = self.base
+            dump(self.config, f, ensure_ascii=False)
+
+
+    def cfgLoad(self) -> dict:
+        try:
+            with open(self.configName, 'r', encoding='utf-8') as f:
+                self.config = load(f)
+                return self.config
+        
+        except FileNotFoundError:
+            self.createBaseConfig()
+            self.config = self.cfgLoad()
+            return self.config
+
+
+
+    def cfgDump(self) -> None:
+        with open(self.configName, 'w', encoding='utf-8') as f:
+            dump({
+                'Grabzone'  : GRABZONE,
+                'IsHoldKey' : IS_HOLDKEY,
+                'HoldKey'   : HOLDKEY,
+                'ToggleKey' : TOGGLEKEY
+            }, f, ensure_ascii=False)
+
+
 class TriggerBot:
 
     def __init__(self) -> None:
-        self._mode       = 0x1
-        self._last_reac  = 0x0
+        self._mode       = 1
+        self._last_reac  = 0
 
 
     def switch(self) -> None:
-        Beep(0xC8, 0x64)
-        if self._mode != 0x5: self._mode += 0x1
-        else: self._mode = 0x0
+        Beep(200, 100)
+        if self._mode != 5: self._mode += 1
+        else: self._mode = 0
 
 
     def color_check(self, red: int, green: int, blue: int) -> bool:
@@ -73,7 +120,7 @@ class TriggerBot:
 
     def grab(self) -> Image:
         with mss() as sct:
-            bbox     = (int(S_HEIGHT / 0x2 - GRABZONE), int(S_WIDTH / 0x2 - GRABZONE), int(S_HEIGHT / 0x2 + GRABZONE), int(S_WIDTH / 0x2 + GRABZONE))
+            bbox     = (int(S_HEIGHT / 2 - GRABZONE), int(S_WIDTH / 2 - GRABZONE), int(S_HEIGHT / 2 + GRABZONE), int(S_WIDTH / 2 + GRABZONE))
             sct_img  = sct.grab(bbox)
             return Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
 
@@ -83,61 +130,79 @@ class TriggerBot:
         pmap        = self.grab()
 
         try:
-            for x in range(0x0, GRABZONE * 0x2):
-                for y in range(0x0, GRABZONE * 0x2):
+            for x in range(0, GRABZONE * 2):
+                for y in range(0, GRABZONE * 2):
                     r, g, b = pmap.getpixel((x, y))
                     if self.color_check(r, g, b): raise FoundEnemy
         
         except FoundEnemy:
-            self._last_reac = int((perf_counter() - start_time) * 0x3E8)
+            self._last_reac = int((perf_counter() - start_time) * 1000)
             windll.user32.mouse_event(0x2, 0x0, 0x0, 0x0, 0x0), windll.user32.mouse_event(0x4, 0x0, 0x0, 0x0, 0x0)
 
-            if self._mode == 0x0: sleep(0.3)
-            elif self._mode == 0x1: sleep(0.25)
-            elif self._mode == 0x2: sleep(0.2)
-            elif self._mode == 0x3: sleep(0.15)
-            elif self._mode == 0x4: sleep(0.1)
-            elif self._mode == 0x5: pass
+            if self._mode == 0: sleep(0.3)
+            elif self._mode == 1: sleep(0.25)
+            elif self._mode == 2: sleep(0.2)
+            elif self._mode == 3: sleep(0.15)
+            elif self._mode == 4: sleep(0.1)
+            elif self._mode == 5: pass
 
 
 def print_banner(bot: TriggerBot) -> None:
     system('cls')
     print(Style.BRIGHT + Fore.CYAN + f'{__author__} Valorant External Cheat {__version__}' + Style.RESET_ALL)
     print('====== Controls ======')
-    print('Trigger Key          :', Fore.YELLOW + TRIGGER_KEY + Style.RESET_ALL)
+    print('Trigger Key          :', Fore.YELLOW + f'{f"HoldKey [{HOLDKEY}]" if IS_HOLDKEY else f"ToggleKey [{TOGGLEKEY}]"}' + Style.RESET_ALL)
     print('Mode Change Key      :', Fore.YELLOW + SWITCH_KEY + Style.RESET_ALL)
     print('Grab Zone Change Key :', Fore.YELLOW + GRABZONE_KEY_UP + '/' + GRABZONE_KEY_DOWN + Style.RESET_ALL)
     print('===== Information ====')
     print('Mode                 :', Fore.CYAN  + MODS[bot._mode] + Style.RESET_ALL)
     print('Grab Zone            :', Fore.CYAN  + str(GRABZONE) + 'x' + str(GRABZONE) + Style.RESET_ALL)
-    print('Trigger Status       :', Fore.GREEN + f'Hold down the "{TRIGGER_KEY}" key' + Style.RESET_ALL)
+    print('Trigger Status       :', Fore.GREEN + f'{f"Hold down the [{HOLDKEY}] key" if IS_HOLDKEY else "Active" if IS_RUNING else Fore.RED + "Passive"}' + Style.RESET_ALL)
     print('Last React Time      :', Fore.CYAN  + str(bot._last_reac) + Style.RESET_ALL + ' ms (' + str((bot._last_reac) / (GRABZONE * GRABZONE)) + 'ms/pix)')
 
 
 if __name__ == "__main__":
     _hash = sha256(f'{random()}'.encode('utf-8')).hexdigest()
     print(_hash), system(f'title {_hash}'), sleep(0.5), init(), system('@echo off'), system('cls')
-    bot = TriggerBot()
+
+    # Config Load
+    cfg        = Config().cfgLoad()
+    GRABZONE   = cfg['Grabzone']
+    IS_HOLDKEY = cfg['IsHoldKey']
+    HOLDKEY    = cfg['HoldKey']
+    TOGGLEKEY  = cfg['ToggleKey']
+    bot        = TriggerBot()
+
     print_banner(bot)
 
-    while 0x1:
+    while True:
         if is_pressed(SWITCH_KEY):
             bot.switch()
             print_banner(bot)
             continue
 
         if is_pressed(GRABZONE_KEY_UP):
-            GRABZONE += 0x1
-            print_banner(bot), Beep(0x190, 0x64) 
+            GRABZONE += 1
+            print_banner(bot), Beep(400, 100) 
             continue  
 
         if is_pressed(GRABZONE_KEY_DOWN):
-            if GRABZONE != 0x1: GRABZONE -= 0x1
-            print_banner(bot), Beep(0x12C, 0x64)
+            if GRABZONE != 1: GRABZONE -= 1
+            print_banner(bot), Beep(300, 100)
             continue
 
-        if is_pressed(TRIGGER_KEY):
-            bot.scan(), print_banner(bot)
-            continue
+        if IS_HOLDKEY:
+            if is_pressed(HOLDKEY):
+                bot.scan(), print_banner(bot)
+                continue
+        
+        else:
+            if is_pressed(TOGGLEKEY):
+                IS_RUNING = not IS_RUNING
+                print_banner(bot), Beep(400, 100)
 
-        sleep(0.001)
+            if IS_RUNING:
+                bot.scan(), print_banner(bot)
+                continue
+
+        sleep(0.0025)
